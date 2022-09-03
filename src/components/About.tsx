9d8a3c87c71bb6054d4a9194ee71588a410dc1bc
Rishabh Kesarwani <rishabhbhs@gmail.com>
2022-09-03 20:07:35 +0530

    Changed layout to make it look better on phone

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ import { ABOUT_CONTENT } from '../constants';
 
 const About = () => {
     return (
-        <Stack>
+        <Stack spacing={2} sx={{margin: '16px 0'}}>
             <Grid container>
                 <Grid item xs={12} md={4}>
                     <img src={SamassyaHope} alt="About" width="100%" height="100%" />
@@ -20,14 +20,14 @@ const About = () => {
                 </Grid>
             </Grid>
             <Grid container>
+                <Grid item xs={12} md={4}>
+                    <img src={SamassyaStory} alt="About" width="100%" height="100%" />
+                </Grid>
                 <Grid item xs={12} md={8}>
                     <Stack alignItems="center" justifyContent="center" spacing={1} sx={{height: '100%'}}>
                         <Typography variant="h3" align="center" sx={{fontFamily: 'Georgia, serif'}}>{ABOUT_CONTENT.about.heading}</Typography>
                         <Typography variant="h6" color="GrayText" align="center" sx={{padding: '0 20px'}}>{ABOUT_CONTENT.about.content}</Typography>
                     </Stack>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <img src={SamassyaStory} alt="About" width="100%" height="100%" />
                 </Grid>
             </Grid>
             <Grid container>
